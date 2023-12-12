312f8ca99bc39ad5b76505d61b93aa197ed8d109
DIILEESHA <dileeeshanawarathna18@gmail.com>
2023-12-13 03:07:51 +0530

    added some animation types

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,24 +1,41 @@
-import React from "react";
+// Header.jsx
+
+import React, { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
 import "./header.css";
-import SentenceAnimation from "./sentences";
-import vide from '../video/vide.mp4'
+import vide from '../video/vide.mp4';
 
 const Header = () => {
+  const [animationComplete, setAnimationComplete] = useState(false);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setAnimationComplete(true);
+      controls.start({ opacity: 1, y: 0, transition: { duration: 1 } });
+    }, 1500); // Set the duration of the animation (3 seconds in this case)
+
+    return () => clearTimeout(timeoutId);
+  }, [controls]);
+
   return (
-    <div className="header_container">
+    <motion.div
+      className={`header_container ${animationComplete ? "animation-complete" : ""}`}
+      animate={controls}
+      initial={{ y: 50, opacity: 0 }}
+    >
       <div className="header_sub">
-        <div className="header_left"></div>
-        <div className="header_middle">
+        <div className={`header_left ${animationComplete ? "fade-in" : ""}`}></div>
+        <div className={`header_middle ${animationComplete ? "fade-in" : ""}`}>
           <h3 className="header_title">creative studio</h3>
           <p className="header_para">
-            WE DESIGN AND BUILD LOVABLE DIGITAL PRODUCTS, BRANDS, AND
-            EXPERIENCES.
+            WE DESIGN AND BUILD LOVABLE DIGITAL PRODUCTS, BRANDS, AND EXPERIENCES.
           </p>
         </div>
-        <div className="header_right"></div>
+        <div className={`header_right ${animationComplete ? "fade-in" : ""}`}></div>
       </div>
 
-      <div className="header_grid">
+      <div className={`header_grid ${animationComplete ? "fade-in" : ""}`}>
         <div className="header_sub_grid">
           <h2 className="grid_text">design</h2>
         </div>
@@ -33,16 +50,19 @@ const Header = () => {
         </div>
       </div>
 
-      <div className="video">
-        <video autoPlay playsInline muted loop crossOrigin="anonymous">
-          <source
-            src={vide}
-            type="video/mp4"
-          />
+      <div className={`video-container ${animationComplete ? "fade-in" : ""}`}>
+        <video
+          autoPlay
+          playsInline
+          muted
+          loop
+          crossOrigin="anonymous"
+          style={{ width: "100%", height: "auto" }}
+        >
+          <source src={vide} type="video/mp4" />
         </video>
       </div>
-
-    </div>
+    </motion.div>
   );
 };
 
