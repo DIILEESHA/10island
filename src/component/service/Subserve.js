94085beefd4a1cc806066303e72f1ff3f15f8ac9
DIILEESHA <dileeeshanawarathna18@gmail.com>
2023-12-13 02:04:47 +0530

    file structure added

--- src/component/service/Subserve.js
+++ src/component/service/Subserve.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./service.css";
 
 const Subserve = () => {
-  return <div>Subserve</div>;
+  return <div className="sub_container">Subserve</div>;
 };
 
 export default Subserve;
