b6ed424f638cca86bd2f0d0af6478e13cec68bb4
DIILEESHA <dileeeshanawarathna18@gmail.com>
2023-12-18 14:24:09 +0530

    change fav

--- src/component/single/Singlepost.js
+++ src/component/single/Singlepost.js
@@ -5,9 +5,11 @@ import { motion, useAnimation } from "framer-motion";
 import "./single.css";
 import { Parallax } from "react-parallax";
 import { useDrag } from "react-use-gesture";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Serve from "../service/serve.json"; // Import your data source
 import ParticleComponent from "../particle/ParticleComponent";
+
+import '../service/service.css'
 
 const SingleComponent = () => {
   const [animationComplete, setAnimationComplete] = useState(false);
@@ -68,6 +70,13 @@ const SingleComponent = () => {
   if (!post) {
     return <p>Post not found</p>;
   }
+
+  const handleClicks = () => {
+    // Scroll to the top of the SingleItemDetail component
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  const parallaxStrength = window.innerHeight / 2;
 
   return (
     <motion.div
@@ -143,11 +152,11 @@ const SingleComponent = () => {
         </div>
       </div>
 
-      <div>
+      <div className="single_img">
         <Parallax
-          className="single_img"
+          className="single_imger"
           style={{}}
-          strength={150}
+          strength={parallaxStrength}
           bgImage={post.img}
         />
       </div>
@@ -157,7 +166,7 @@ const SingleComponent = () => {
         data-aos="fade-up-right"
         data-aos-duration="2000"
       >
-        <p className="bottom_para rr">
+        <p className="bottom_para rd">
           <span className="cooler">Quantum Wallet</span>
           is a revolutionary hardware wallet for your digital assets — most
           secure by a quantum leap.
@@ -182,7 +191,105 @@ const SingleComponent = () => {
         </video>
       </div>
 
-      <div>1</div>
+      <div className="single_stack">
+        <div className="single_stack_left">
+          <h2
+            className="values_title"
+            data-aos="fade-up-left"
+            data-aos-duration="2000"
+          >
+            TECHNOLOGIES
+          </h2>
+        </div>
+        <div className="hi">
+        <div className="single_stack_right">
+          <div className="single_sub_stack_right">
+            <div className="demo" data-aos="fade-up" data-aos-duration="2000">
+              <h2 className="sub_culture_topic fdc">Javascript framework</h2>
+              <p className="sub_culture_para">React</p>
+            </div>{" "}
+            <div className="demo" data-aos="fade-up" data-aos-duration="2000">
+              <h2 className="sub_culture_topic">Web framework</h2>
+              <p className="sub_culture_para">Gatsby</p>
+            </div>
+            <div className="demo" data-aos="fade-up" data-aos-duration="2000">
+              <h2 className="sub_culture_topic">Animation library</h2>
+              <p className="sub_culture_para">Framer Motion</p>
+            </div>
+            <div className="demo" data-aos="fade-up" data-aos-duration="2000">
+              <h2 className="sub_culture_topic">Graphics library</h2>
+              <p className="sub_culture_para">ThreeJS</p>
+            </div>
+          </div>
+        </div>
+        <div className="single_stack_right">
+          <div className="single_sub_stack_right">
+            <div className="demo" data-aos="fade-up" data-aos-duration="2000">
+              <h2 className="sub_culture_topic">Database services</h2>
+              <p className="sub_culture_para">Firebase</p>
+            </div>{" "}
+            <div className="demo" data-aos="fade-up" data-aos-duration="2000">
+              <h2 className="sub_culture_topic">User analytics</h2>
+              <p className="sub_culture_para">Google Tag Manager</p>
+            </div>
+            <div className="demo" data-aos="fade-up" data-aos-duration="2000">
+              <h2 className="sub_culture_topic">CMS</h2>
+              <p className="sub_culture_para">Prismic</p>
+            </div>
+            <div className="demo" data-aos="fade-up" data-aos-duration="2000">
+              <h2 className="sub_culture_topic">Hosting platform</h2>
+              <p className="sub_culture_para">Netlify</p>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+      <h2
+        className="values_title"
+        data-aos="fade-up-left"
+        data-aos-duration="2000"
+        style={{ marginBottom: "20px" }}
+      >
+        MORE WORK
+      </h2>
+      <div className="main_cards">
+        {Serve.slice(0, 2).map((serves, index) => (
+          <Link
+            className="linka"
+            to={`/single/${index}`}
+            key={index}
+            onClick={handleClicks}
+          >
+            <div
+              className="sub_img_card"
+              data-aos="fade-up"
+              data-aos-anchor-placement={
+                index % 2 === 0 ? "top-bottom" : "top-center"
+              }
+              data-aos-duration="2000"
+            >
+              <img className="gusa" src={serves.img_link} alt="" />
+
+              <div
+                className="texter_align"
+                data-aos="fade-up-left"
+                data-aos-anchor-placement={
+                  index % 2 === 0 ? "top-bottom" : "top-center"
+                }
+                data-aos-duration="2000"
+              >
+                <div className="line">
+                  <h2 className="main_card_texter1">{serves.name}</h2>
+                  <div className="linerr">
+                    <div className="liner"></div>
+                    <h2 className="main_card_texter2">{serves.category}</h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </motion.div>
   );
 };
